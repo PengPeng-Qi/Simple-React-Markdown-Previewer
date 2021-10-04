cc1d12ea544f73b24f6791504f743a9ad5f62d12
PengPengQ <1747447034@qq.com>
2021-10-05 00:31:59 +0800

    Text GitHub Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Editor from './components/Editor'
 import Preview from './components/Preview'
+import Editor from './components/Editor'
 import './App.css'
 
 export default class App extends Component {
